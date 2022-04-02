b528687bd97ef8fe711098e34c70ad94ea8a267b
Steven Garberg <steven.garberg.jr@gmail.com>
2022-04-02 00:40:57 -0400

    Added ColorRandomizer

---
+++ Unity/Assets/Scripts/Utilities/ColorRandomizer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace.Utilities
+{
+    public static class ColorRandomizer
+    {
+        public static Color GetRandomPresetColor()
+        {
+            var colors = new[]
+            {
+                Color.blue, Color.cyan, Color.green, Color.magenta, Color.red, Color.yellow
+            };
+            var randomNumber = Random.Range(0, colors.Length);
+            return colors[randomNumber];
+        }
+    }
+}
