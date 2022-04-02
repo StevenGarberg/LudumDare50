ff012dc4edbeafb3192848164c1c3a1c24bc6945
Steven Garberg <steven.garberg.jr@gmail.com>
2022-04-02 00:54:34 -0400

    Added PlayerController

---
+++ Unity/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace LudumDare50.Controllers
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerController : MonoBehaviour
+    {
+        private Rigidbody2D _rigidbody2D;
+
+        private void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                // TODO: Move left
+            }
+            else if (Input.GetKey(KeyCode.R))
+            {
+                // TODO: Move right
+            }
+        }
+    }
+}
