79b91e54c07d9506f3d9413581d04c5fcffd2251
Steven Garberg <steven.garberg.jr@gmail.com>
2022-04-03 19:48:31 -0400

    Added plugins

---
+++ Unity/Assets/Plugins/crosstales/TurboSwitch/Editor/Task/Launch.cs
@@ -0,0 +1,27 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+
+namespace Crosstales.TPS.Task
+{
+   /// <summary>Show the configuration window on the first launch.</summary>
+   [InitializeOnLoad]
+   public static class Launch
+   {
+      #region Constructor
+
+      static Launch()
+      {
+         bool launched = EditorPrefs.GetBool(Util.Constants.KEY_LAUNCH);
+
+         if (!launched)
+         {
+            EditorIntegration.ConfigWindow.ShowWindow(0);
+            EditorPrefs.SetBool(Util.Constants.KEY_LAUNCH, true);
+         }
+      }
+
+      #endregion
+   }
+}
+#endif
+// © 2017-2020 crosstales LLC (https://www.crosstales.com)
