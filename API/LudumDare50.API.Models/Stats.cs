debec34ec17b5b629a90ab3783387dda2ad5ef7c
Christopher Plummer <chrisplummer0194@gmail.com>
2022-04-03 15:35:05 -0400

    Updated Stats with constructor mapping and added StatsService

--- API/LudumDare50.API.Models/Stats.cs
+++ API/LudumDare50.API.Models/Stats.cs
@@ -1,6 +1,17 @@
-﻿namespace LudumDare50.API.Models;
+﻿using System;
+
+namespace LudumDare50.API.Models;
 
 public class Stats : BaseResource
 {
-    
+    public string GameName { get; set; }
+    public Stats() {}
+
+    public Stats(string ownerId, string gameName, Stats request)
+    {
+        this.Id = Guid.NewGuid().ToString();
+        this.OwnerId = ownerId;
+        this.GameName = gameName;
+        //TODO: Map incoming request stats to constructed stats
+    }
 }
