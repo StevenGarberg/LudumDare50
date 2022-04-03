6ffde1b9d567edd55299894893f15dd695289dac
Christopher Plummer <chrisplummer0194@gmail.com>
2022-04-03 16:09:27 -0400

    Update Stats model

--- API/LudumDare50.API.Models/Stats.cs
+++ API/LudumDare50.API.Models/Stats.cs
@@ -7,11 +7,12 @@ public class Stats : BaseResource
     public string GameName { get; set; }
     public Stats() {}
 
-    public Stats(string ownerId, string gameName, Stats request)
+    public Stats(string gameName, string ownerId, Stats request)
     {
         Id = string.IsNullOrEmpty(request.Id) ? Guid.NewGuid().ToString() : request.Id;
         OwnerId = ownerId;
         GameName = gameName;
+        Version = 1;
         //TODO: Map incoming request stats to constructed stats
     }
 }
