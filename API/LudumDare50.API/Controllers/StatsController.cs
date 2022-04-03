85c8b6029a34dbe3b92c4d231d26fc388ecffe37
Christopher Plummer <chrisplummer0194@gmail.com>
2022-04-03 15:45:06 -0400

    csproj update and added StatsController

---
+++ API/LudumDare50.API/Controllers/StatsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LudumDare50.API.Controllers;
+
+[ApiController]
+[Route("games/{gameName}/client/{id:guid}/stats")]
+public class StatsController : ControllerBase
+{
+    
+}
