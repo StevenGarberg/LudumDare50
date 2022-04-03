8f49b49763876a6408b77e699934f61406ca67c7
Christopher Plummer <chrisplummer0194@gmail.com>
2022-04-03 16:09:50 -0400

    Dependency Injection

--- API/LudumDare50.API/Program.cs
+++ API/LudumDare50.API/Program.cs
@@ -1,3 +1,8 @@
+using LudumDare50.API.Models;
+using LudumDare50.API.Repositories;
+using LudumDare50.API.Repositories.Interfaces;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var mongoDbConnectionString = builder.Configuration["ConnectionStrings:Mongo"];
+builder.Services.AddScoped<IMongoClient, MongoClient>(_ => new MongoClient(MongoClientSettings.FromConnectionString(mongoDbConnectionString)));
+
+builder.Services.AddScoped<IMongoRepository<Stats>, StatsRepository>();
 
 var app = builder.Build();
 
