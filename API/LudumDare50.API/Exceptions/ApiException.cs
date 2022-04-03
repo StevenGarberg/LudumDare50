46f62c1115f19469eafd103be196cd97de0ccb34
Christopher Plummer <chrisplummer0194@gmail.com>
2022-04-03 15:45:31 -0400

    Added ApiException and ApiExceptionFilter

---
+++ API/LudumDare50.API/Exceptions/ApiException.cs
@@ -0,0 +1,12 @@
+﻿namespace LudumDare50.API.Exceptions;
+
+public abstract class ApiException : Exception
+{
+    public int? StatusCode { get; }
+
+    protected ApiException() { }
+    protected ApiException(string message, int? statusCode = null) : base(message)
+    {
+        StatusCode = statusCode ?? StatusCode;
+    }
+}
