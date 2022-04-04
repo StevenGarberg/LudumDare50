d7f161b9a2e0abe7c134d85274ba97df87e8ce20
Christopher Plummer <chrisplummer0194@gmail.com>
2022-04-04 14:34:57 -0400

    API models update

--- API/LudumDare50.API.Models/BaseResource.cs
+++ API/LudumDare50.API.Models/BaseResource.cs
@@ -8,11 +8,11 @@ public class BaseResource
 {
     [BsonId] public string Id { get; set; }
 
-    public string OwnerId { get; set; }
-
     [ConcurrencyCheck] public int Version { get; set; }
 
     public DateTime CreatedAt { get; set; }
 
     public DateTime UpdatedAt { get; set; }
+    
+    public string GameName { get; set; }
 }
